375391860aad63d7e86046d57cbee330a6e827ae
Vincent Jardel <vincent.jardel@dolby.com>
2020-02-17 10:41:28 +0100

    Force Swift to version 5.0

--- add-swift-support.js
+++ add-swift-support.js
@@ -138,19 +138,23 @@ module.exports = context => {
               console.log('Update IOS build setting LD_RUNPATH_SEARCH_PATHS to: @executable_path/Frameworks', 'for build configuration', buildConfig.name);
             }
 
-            if (typeof xcodeProject.getBuildProperty('SWIFT_VERSION', buildConfig.name) === 'undefined') {
-              if (config.getPreference('UseLegacySwiftLanguageVersion', 'ios')) {
-                xcodeProject.updateBuildProperty('SWIFT_VERSION', '2.3', buildConfig.name);
-                console.log('Use legacy Swift language version', buildConfig.name);
-              } else if (config.getPreference('UseSwiftLanguageVersion', 'ios')) {
-                const swiftVersion = config.getPreference('UseSwiftLanguageVersion', 'ios');
-                xcodeProject.updateBuildProperty('SWIFT_VERSION', swiftVersion, buildConfig.name);
-                console.log('Use Swift language version', swiftVersion);
-              } else {
-                xcodeProject.updateBuildProperty('SWIFT_VERSION', '5.0', buildConfig.name);
-                console.log('Update SWIFT version to 5.0', buildConfig.name);
-              }
-            }
+            // if (typeof xcodeProject.getBuildProperty('SWIFT_VERSION', buildConfig.name) === 'undefined') {
+            //   if (config.getPreference('UseLegacySwiftLanguageVersion', 'ios')) {
+            //     xcodeProject.updateBuildProperty('SWIFT_VERSION', '2.3', buildConfig.name);
+            //     console.log('Use legacy Swift language version', buildConfig.name);
+            //   } else if (config.getPreference('UseSwiftLanguageVersion', 'ios')) {
+            //     const swiftVersion = config.getPreference('UseSwiftLanguageVersion', 'ios');
+            //     xcodeProject.updateBuildProperty('SWIFT_VERSION', swiftVersion, buildConfig.name);
+            //     console.log('Use Swift language version', swiftVersion);
+            //   } else {
+            //     xcodeProject.updateBuildProperty('SWIFT_VERSION', '5.0', buildConfig.name);
+            //     console.log('Update SWIFT version to 5.0', buildConfig.name);
+            //   }
+            // }
+
+            // Force Swift Version on 5.0
+            xcodeProject.updateBuildProperty('SWIFT_VERSION', '5.0', buildConfig.name);
+            console.log('Update SWIFT version to 5.0', buildConfig.name);
 
             if (buildConfig.name === 'Debug') {
               if (xcodeProject.getBuildProperty('SWIFT_OPTIMIZATION_LEVEL', buildConfig.name) !== '"-Onone"') {
